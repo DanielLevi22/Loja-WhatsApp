6785bc5d06666b20668ec8381f0e532aba4a5057
Daniel Levi <daniellevi000@gmail.com>
2024-03-08 22:11:29 -0300

    adicionando sincronidmos entre as tabs

--- src/components/products/tab.tsx
+++ src/components/products/tab.tsx
@@ -1,25 +1,56 @@
 import { getAllProducts } from "@/services/product";
 import { Tabs, TabsList,TabsContent ,TabsTrigger} from "../ui/tabs";
+import { Product } from "@/types/product";
 
+type Tab = {
+  title: string
+  value: string
+  products: Product[]
+}
 
 export async function ProductsTab() {
   const product = await getAllProducts();
-  return (
-    <Tabs defaultValue="tab-1">
-      <TabsList className="flex">
-        <TabsTrigger value="tab-1" className="flex-1" >tab-1</TabsTrigger>
-        <TabsTrigger value="tab-2" className="flex-1" >tab-2</TabsTrigger>
-      </TabsList>
-      <TabsContent value="tab-1" className="mt-6">
-          contentudo 1
-      </TabsContent>
-      <TabsContent value="tab2">
-          contentudo 2
-      </TabsContent>
-      <TabsContent value="tab3">
-          contentudo 3
-      </TabsContent>
 
+  const tabs:Tab[]= [ 
+    {
+      title: 'Sushi',
+      value: 'sushi',
+      products: []
+    },
+    {
+      title: 'Temaki',
+      value: 'temaki',
+      products: []
+    },
+    {
+      title: 'Combinados',
+      value: 'combinados',
+      products: []
+    },
+    {
+      title: 'Bebidas',
+      value: 'beverage',
+      products: []
+    },
+  ]
+  return (
+    <Tabs defaultValue="sushi">
+      <TabsList className="flex">
+      {
+        tabs.map(item => (
+        <TabsTrigger key={item.title} value={item.value} className="flex-1" >{item.title}</TabsTrigger>
+        ))
+      }
+
+      </TabsList>
+      {tabs.map(item => (
+        <TabsContent key={item.title} value={item.value} className="mt-6">
+            contentudo 2
+        </TabsContent>
+
+      ))}
+     
+  
     </Tabs>
   )
 }
